d8a7b927908e324bf4f5faa9083259d3c44f221e
Dhanasekar <sendmailtodhana@gmail.com>
2021-05-21 21:10:17 +0530

    reverted Code for auto detect

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'dart:async';
-
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:fuelflex/config/text_strings.dart';
 import 'package:fuelflex/providers/data_provider.dart';
 import 'package:fuelflex/providers/service_providers.dart';
-import 'package:fuelflex/screens/mainPage_screen.dart';
+import 'package:fuelflex/screens/MainPage_screen.dart';
 import 'package:fuelflex/screens/insertCardPage_screen.dart';
 import 'package:fuelflex/screens/saleMenuPage_screen.dart';
 import 'package:fuelflex/screens/splash_screen.dart';
@@ -31,32 +27,23 @@ void main() async {
   ));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
- final _navigatorKey = GlobalKey<NavigatorState>();
-  
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-    primarySwatch: Colors.blue,
-        ),
-        home: SplashScreen(),
-        navigatorKey: _navigatorKey,
-        routes: {
-    SplashScreen.routeName: (context) => SplashScreen(),
-    LoginPageScreen.routeName: (context) => LoginPageScreen(),
-    MainPageScreen.routeName: (context) => MainPageScreen(),
-    TagTapPageScreen.routeName: (context) => TagTapPageScreen(),
-    SaleMenuPageScreen.routeName: (context) => SaleMenuPageScreen(),
-    InsertCardPageScreen.routeName: (context) => InsertCardPageScreen(),
-        },
-      );
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+      routes: {
+        SplashScreen.routeName: (context) => SplashScreen(),
+        LoginPageScreen.routeName: (context) => LoginPageScreen(),
+        MainPageScreen.routeName: (context) => MainPageScreen(),
+        TagTapPageScreen.routeName: (context) => TagTapPageScreen(),
+        SaleMenuPageScreen.routeName: (context) => SaleMenuPageScreen(),
+        InsertCardPageScreen.routeName: (context) => InsertCardPageScreen(),
+      },
+    );
   }
 }
--- lib/screens/MainPage_screen.dart
+++ lib/screens/MainPage_screen.dart
@@ -13,83 +13,54 @@ class MainPageScreen extends StatefulWidget {
 }
 
 class _MainPageScreenState extends State<MainPageScreen> {
-  Timer _timer;
-
   @override
   void initState() {
     super.initState();
-    _initializeTimer();
   }
-
-  void _initializeTimer() {
-    if (_timer != null) {
-      _timer.cancel();
-    }
-
-    _timer = Timer(const Duration(seconds: 15), _logOutUser);
-  }
-
-  void _logOutUser() {
-    _timer?.cancel();
-    _timer = null;
-
-    // Popping all routes and pushing welcome screen
-    Navigator.of(context)
-        .pushNamedAndRemoveUntil(TextStrings.appLoginScreenPath, (_) => false);
-  }
-
-  void _handleUserInteraction([_]) {
-    _initializeTimer();
-  }
-
   @override
   Widget build(BuildContext context) {
     Size _size = MediaQuery.of(context).size;
-    return GestureDetector(
-      behavior: HitTestBehavior.translucent,
-            onTap: _handleUserInteraction,
-      child: Scaffold(
-        body: BackgroundWidget(
-          imagePath: TextStrings.appAssetBackgroundPlainPath,
-          child: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Flexible(
-                child: Container(
-                  height: _size.height * 0.20,
-                  width: _size.width * 0.50,
-                  child: Image(
-                    image: AssetImage(
-                      TextStrings.appAssetOlaCardColorLogoPath,
-                    ),
-                    fit: BoxFit.contain,
+    return Scaffold(
+      body: BackgroundWidget(
+        imagePath: TextStrings.appAssetBackgroundPlainPath,
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(
+              child: Container(
+                height: _size.height * 0.20,
+                width: _size.width * 0.50,
+                child: Image(
+                  image: AssetImage(
+                    TextStrings.appAssetOlaCardColorLogoPath,
                   ),
+                  fit: BoxFit.contain,
                 ),
               ),
-              Expanded(
-                child: Container(
-                  width: _size.width * 0.75,
-                  child: ListView.builder(
-                    itemBuilder: (_, index) {
-                      return Container(
-                        padding: const EdgeInsets.all(10.0),
-                        child: SubMenuButtonWidget(
-                          title: MenuItems().menuItems[index]["title"],
-                          onPressed: () {
-                            Navigator.of(context).pushNamed(
-                                MenuItems().menuItems[index]["onPressed"]);
-                          },
-                        ),
-                      );
-                    },
-                    itemCount: MenuItems().menuItems.length,
-                  ),
+            ),
+            Expanded(
+              child: Container(
+                width: _size.width * 0.75,
+                child: ListView.builder(
+                  itemBuilder: (_, index) {
+                    return Container(
+                      padding: const EdgeInsets.all(10.0),
+                      child: SubMenuButtonWidget(
+                        title: MenuItems().menuItems[index]["title"],
+                        onPressed: () {
+                          Navigator.of(context).pushNamed(
+                              MenuItems().menuItems[index]["onPressed"]);
+                        },
+                      ),
+                    );
+                  },
+                  itemCount: MenuItems().menuItems.length,
                 ),
               ),
-            ],
-          )),
-        ),
+            ),
+          ],
+        )),
       ),
     );
   }
